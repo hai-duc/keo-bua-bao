da650046ebc134900d68a8835add1dca262008be
hai-duc <vuhaiduc@gmail.com>
2020-08-14 11:30:15 +0700

    commit lan 2

---
+++ keo-bua-bao/precache-manifest.55611c459f65a3a54f0fa0bb6816a4e9.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "73a23bef9a8df50d7d39b9f0c8a2b83e",
+    "url": "/hai-duc/keo-bua-bao/index.html"
+  },
+  {
+    "revision": "1f89709a2e51b008ad36",
+    "url": "/hai-duc/keo-bua-bao/static/css/main.7716a263.chunk.css"
+  },
+  {
+    "revision": "24573f031257ff97bdac",
+    "url": "/hai-duc/keo-bua-bao/static/js/2.5137490e.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/hai-duc/keo-bua-bao/static/js/2.5137490e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1f89709a2e51b008ad36",
+    "url": "/hai-duc/keo-bua-bao/static/js/main.29cfacb7.chunk.js"
+  },
+  {
+    "revision": "7d2551c0152341a3aab5",
+    "url": "/hai-duc/keo-bua-bao/static/js/runtime-main.7aa4bf13.js"
+  },
+  {
+    "revision": "6df7cfded512890755f5b93acf26704d",
+    "url": "/hai-duc/keo-bua-bao/static/media/background.6df7cfde.jpg"
+  },
+  {
+    "revision": "b91b400f600c23e98a02e8a469622159",
+    "url": "/hai-duc/keo-bua-bao/static/media/p_button.b91b400f.png"
+  },
+  {
+    "revision": "5c54f18e7dac9addd8e1708c38ca7414",
+    "url": "/hai-duc/keo-bua-bao/static/media/r_button.5c54f18e.png"
+  },
+  {
+    "revision": "3bf31555ebbd19aed7bfe0c3e7ac1155",
+    "url": "/hai-duc/keo-bua-bao/static/media/s_button.3bf31555.png"
+  }
+]);
